971ba19ab0dad64484742a26d6b5532a6924f9d3
usagreatus <tolyaaaa@gmail.com>
2023-11-26 21:50:13 -0500

    Initial commit

---
+++ src/RomanNumerals.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+/**
+ * RomanNumerals class
+ */
+class RomanNumerals
+{
+    const NUMERALS = [
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+    ];
+
+    /**
+     * @param $number
+     * @return false|string
+     */
+    public static function generate($number)
+    {
+        if ($number <= 0 || $number >= 4000) {
+            return false;
+        }
+
+        $result = '';
+
+        foreach (static::NUMERALS as $numeral => $arabic) {
+            for (; $number >= $arabic; $number -= $arabic) {
+                $result .= $numeral;
+            }
+        }
+
+        return $result;
+    }
+}
